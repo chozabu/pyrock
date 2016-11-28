8d170e7b29326e9c0b7a48b1f2c29f5be4627967
chozabu <chozabu@gmail.com>
2016-11-28 16:08:33 +0000

    tidy up debugprinter

--- debugprint.py
+++ debugprint.py
@@ -9,15 +9,23 @@ printcount = 0
 class TracePrints(object):
 	def __init__(self):
 		self.stdout = sys.stdout
+		self.newline = True
 	def write(self, s):
 		global printcount
-		printcount +=1
 		from settings import machine_name
-		#self.stdout.write(s+"\n")
-		strs = "(" + machine_name + " - " + str(printcount) + ")" + str(s)#+"\n"
+		if self.newline:
+			#self.stdout.write(s+"\n")
+			#stack = traceback.extract_stack()
+			#stackout = stack[-2]#[0:10]
+			#self.stdout.write('--"'+machine_name+'"'+stackout[0]+'", line '+str(stackout[1])+', in '+stackout[2]+'\n')
+			printcount +=1
+			strs = "(" + machine_name + " - " + str(printcount) + ")" + str(s)#+"\n"
+		else:
+			strs = s
 		self.stdout.write(strs)
-		#stack = traceback.extract_stack()
-		#stackout = stack[-2]#[0:10]
-		#self.stdout.write('--"'+machine_name+'"'+stackout[0]+'", line '+str(stackout[1])+', in '+stackout[2]+'\n')
+		if s == '\n':
+			self.newline=True
+		else:
+			self.newline=False
 
 sys.stdout = TracePrints()
